ce5e264f71c55b4b71d7945b6b2b17068bc791c4
seooyuun <seoyuni2002@gmail.com>
2025-07-09 20:00:37 +0900

    Design: 폰트명 변경

--- qrumble/src/styles/GlobalStyle.js
+++ qrumble/src/styles/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     @font-face {
-        font-family: 'Pretendard-Regular';
+        font-family: 'Pretendard';
         src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
         font-weight: 400;
         font-style: normal;
