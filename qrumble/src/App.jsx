f26d58692c5d67f7a056a0bd2b1141e9144f5733
otwaylee <ahyeong858@gmail.com>
2025-07-15 01:07:30 +0900

    Refactor: 스플래쉬 분리

--- qrumble/src/App.jsx
+++ qrumble/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import styled, { ThemeProvider } from 'styled-components';
-import Logo from './assets/svgs/logo.svg?react';
+import { ThemeProvider } from 'styled-components';
 import router from './router';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
 function App() {
-  const [showSplash, setShowSplash] = useState(true);
-  const handleSplashClick = () => {
-    setShowSplash(false);
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyle />
-        {showSplash ? (
-          <LogoImg onClick={handleSplashClick} />
-        ) : (
-          <RouterProvider router={router} />
-        )}
+        <RouterProvider router={router} />
       </>
     </ThemeProvider>
   );
 }
 
-const LogoImg = styled(Logo)`
-  margin: 13.75rem 6.6875rem;
-  width: 146px;
-  height: 179px;
-  cursor: pointer;
-`;
 export default App;
--- qrumble/src/router.jsx
+++ qrumble/src/router.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Login from './login/pages/Login';
 import HomePage from './home/HomePage';
+import Splash from './Splash';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Splash />,
+  },
   {
     path: '/login',
     element: <Login />,
