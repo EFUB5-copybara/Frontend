eb950e5f8c7a15444000bbbf791d7d35ed0f0ddb
otwaylee <ahyeong858@gmail.com>
2025-07-15 03:28:55 +0900

    Chore: 경로

--- qrumble/src/router.jsx
+++ qrumble/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Login from './login/pages/Login';
 import HomePage from './home/HomePage';
 import Splash from './Splash';
+import FindId from './login/pages/FindId';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/find-id',
+    element: <FindId />,
   },
   {
     path: '/home',
