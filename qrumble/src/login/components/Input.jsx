12cab2c3a1afadea4bdfa90b3c0d3f9e5904ca21
otwaylee <ahyeong858@gmail.com>
2025-07-15 15:35:54 +0900

    Feat: input 컴포넌트

--- qrumble/src/login/components/Input.jsx
+++ qrumble/src/login/components/Input.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Input = styled.input`
+  width: 100%;
   padding: 8px;
   border: none;
   border-bottom: 0.0625rem solid ${({ theme }) => theme.colors.brown3};
