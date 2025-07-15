91c6e61e2d111d90441dc99d867913bcb5bc8bd1
otwaylee <ahyeong858@gmail.com>
2025-07-15 15:46:03 +0900

    Design: 로그인, 비밀번호 변경, 비밀번호 재설정 페이지

--- qrumble/src/login/pages/Login.jsx
+++ qrumble/src/login/pages/Login.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
-
 import styled from 'styled-components';
-import Logo from '@/assets/svgs/logo.svg?react';
 import LoginSupportLinks from '../components/LoginSupportLinks';
+import LogoImg from '../components/Logo';
 
 export default function Login() {
   return (
-    <Wrapper>
+    <Container>
       <LogoImg />
       <LoginForm />
       <LoginSupportLinks />
-    </Wrapper>
+    </Container>
   );
 }
 
-const Wrapper = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 5.25rem;
   margin: 5.3125rem 1.25rem;
-`;
-
-const LogoImg = styled(Logo)`
-  width: 146px;
-  height: 179px;
 `;
