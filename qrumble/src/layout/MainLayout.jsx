4485626de5bf7f7079c7c6cdfd14443615a25114
otwaylee <ahyeong858@gmail.com>
2025-07-22 15:30:09 +0900

    feat: layout 설정

---
+++ qrumble/src/layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import NavigationBar from '@/components/NavigationBar';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function MainLayout() {
+  return (
+    <div>
+      <LayoutWrapper>
+        <Outlet />
+      </LayoutWrapper>
+      <NavigationBar />
+    </div>
+  );
+}
+
+const LayoutWrapper = styled.main`
+  width: 360px;
+`;
