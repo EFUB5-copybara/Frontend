cbfaa3d398a73d0128a8993212008404ab7725ae
seooyuun <seoyuni2002@gmail.com>
2025-07-22 14:55:36 +0900

    feat: MyPage/Bookmark

--- qrumble/src/mypage/pages/BookmarkPage.jsx
+++ qrumble/src/mypage/pages/BookmarkPage.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import MyPageTopBar from "../components/MyPageTopBar";
+import Article from "../components/Article";
 
 function BookmarkPage() {
   return (
     <Wrapper>
       <MyPageTopBar title="북마크" />
+      <OrderWrapper>
+        <LatestButton>최신순</LatestButton>
+        <PopularityButton>인기순</PopularityButton>
+      </OrderWrapper>
+      <ArticleWrapper>
+        <Article />
+        <Article />
+        <Article />
+      </ArticleWrapper>
     </Wrapper>
   );
 }
@@ -16,4 +26,37 @@ const Wrapper = styled.div`
   width: 360px;
   height: 800px;
   position: relative;
+`;
+
+const OrderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 10px;
+  height: 31px;
+  margin: 9px 20px 26px 20px;
+  font-family: ${({ theme }) => theme.fonts.c14M};
+`;
+
+const LatestButton = styled.button`
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.brown3};
+  border-radius: 100px;
+  width: 83px;
+`;
+
+const PopularityButton = styled.button`
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.brown3};
+  border-radius: 100px;
+  width: 83px;
+`;
+
+const ArticleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 14px;
+  margin: 0 20px 0 20px;
 `;
