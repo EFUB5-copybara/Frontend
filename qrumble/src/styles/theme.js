593ba5f0b5b08abb981d6e78319828a8d3d99d26
otwaylee <ahyeong858@gmail.com>
2025-07-26 15:28:48 +0900

    refactor: theme.js 믹스인 형식 수정

--- qrumble/src/styles/theme.js
+++ qrumble/src/styles/theme.js
@@ -1,3 +1,5 @@
+import { css } from 'styled-components';
+
 const theme = {
   colors: {
     white: '#FFFFFF',
@@ -12,105 +14,104 @@ const theme = {
     brown3: '#D8C4B1',
     error: '#DE4E3E',
   },
-  fontFamily: "'Pretendard', 'Nunito'",
 
   fonts: {
-    d32SB: {
-      fontFamily: 'Pretendard',
-      fontSize: '32px',
-      fontWeight: 600,
-      lineHeight: '42px',
-    },
-    d24SB: {
-      fontFamily: 'Pretendard',
-      fontSize: '24px',
-      fontWeight: 600,
-      lineHeight: '34px',
-    },
-    t20SB: {
-      fontFamily: 'Pretendard',
-      fontSize: '20px',
-      fontWeight: 600,
-      lineHeight: '28px',
-    },
-    sub16SB: {
-      fontFamily: 'Pretendard',
-      fontSize: '16px',
-      fontWeight: 600,
-      lineHeight: '26px',
-    },
-    sub14SB: {
-      fontFamily: 'Pretendard',
-      fontSize: '14px',
-      fontWeight: 600,
-      lineHeight: '22px',
-    },
-    b18M: {
-      fontFamily: 'Pretendard',
-      fontSize: '18px',
-      fontWeight: 500,
-      lineHeight: '26px',
-    },
-    b16B: {
-      fontFamily: 'Pretendard',
-      fontSize: '16px',
-      fontWeight: 700,
-      lineHeight: '26px',
-    },
-    b16M: {
-      fontFamily: 'Pretendard',
-      fontSize: '16px',
-      fontWeight: 500,
-      lineHeight: '26px',
-    },
-    c14M: {
-      fontFamily: 'Pretendard',
-      fontSize: '14px',
-      fontWeight: 500,
-      lineHeight: '22px',
-    },
-    c14L: {
-      fontFamily: 'Pretendard',
-      fontSize: '14px',
-      fontWeight: 300,
-      lineHeight: '22px',
-    },
-    c12M: {
-      fontFamily: 'Pretendard',
-      fontSize: '12px',
-      fontWeight: 500,
-      lineHeight: '19px',
-    },
-    c12L: {
-      fontFamily: 'Pretendard',
-      fontSize: '12px',
-      fontWeight: 300,
-      lineHeight: '19px',
-    },
-    nd18B: {
-      fontFamily: 'Nunito',
-      fontSize: '18px',
-      fontWeight: 700,
-      lineHeight: '26px',
-    },
-    nd18SB: {
-      fontFamily: 'Nunito',
-      fontSize: '18px',
-      fontWeight: 600,
-      lineHeight: '26px',
-    },
-    ns16M: {
-      fontFamily: 'Nunito',
-      fontSize: '16px',
-      fontWeight: 500,
-      lineHeight: '26px',
-    },
-    ns14SB: {
-      fontFamily: 'Nunito',
-      fontSize: '14px',
-      fontWeight: 600,
-      lineHeight: '22px',
-    },
+    d32SB: css`
+      font-family: 'Pretendard';
+      font-size: 32px;
+      font-weight: 600;
+      line-height: 42px;
+    `,
+    d24SB: css`
+      font-family: 'Pretendard';
+      font-size: 24px;
+      font-weight: 600;
+      line-height: 34px;
+    `,
+    t20SB: css`
+      font-family: 'Pretendard';
+      font-size: 20px;
+      font-weight: 600;
+      line-height: 28px;
+    `,
+    sub16SB: css`
+      font-family: 'Pretendard';
+      font-size: 16px;
+      font-weight: 600;
+      line-height: 26px;
+    `,
+    sub14SB: css`
+      font-family: 'Pretendard';
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 22px;
+    `,
+    b18M: css`
+      font-family: 'Pretendard';
+      font-size: 18px;
+      font-weight: 500;
+      line-height: 26px;
+    `,
+    b16B: css`
+      font-family: 'Pretendard';
+      font-size: 16px;
+      font-weight: 700;
+      line-height: 26px;
+    `,
+    b16M: css`
+      font-family: 'Pretendard';
+      font-size: 16px;
+      font-weight: 500;
+      line-height: 26px;
+    `,
+    c14M: css`
+      font-family: 'Pretendard';
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 22px;
+    `,
+    c14L: css`
+      font-family: 'Pretendard';
+      font-size: 14px;
+      font-weight: 300;
+      line-height: 22px;
+    `,
+    c12M: css`
+      font-family: 'Pretendard';
+      font-size: 12px;
+      font-weight: 500;
+      line-height: 19px;
+    `,
+    c12L: css`
+      font-family: 'Pretendard';
+      font-size: 12px;
+      font-weight: 300;
+      line-height: 19px;
+    `,
+    nd18B: css`
+      font-family: 'Nunito';
+      font-size: 18px;
+      font-weight: 700;
+      line-height: 26px;
+    `,
+    nd18SB: css`
+      font-family: 'Nunito';
+      font-size: 18px;
+      font-weight: 600;
+      line-height: 26px;
+    `,
+    ns16M: css`
+      font-family: 'Nunito';
+      font-size: 16px;
+      font-weight: 500;
+      line-height: 26px;
+    `,
+    ns14SB: css`
+      font-family: 'Nunito';
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 22px;
+    `,
   },
 };
 
